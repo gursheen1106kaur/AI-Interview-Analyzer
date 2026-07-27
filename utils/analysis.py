e45170281dc9c8552577c671888542a2642217e1
Gursheen kaur Dhillon <gursheen1106kaur@gmail.com>
2026-07-27 11:25:32 +0530

    Replace PyDub with Mutagen

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,4 +1,4 @@
-from pydub import AudioSegment
+from mutagen.mp3 import MP3
 from textblob import TextBlob
 
 
@@ -7,40 +7,26 @@ def count_words(text):
 
 
 def get_audio_duration(audio_file):
-    audio = AudioSegment.from_file(audio_file)
-    return len(audio) / 1000
+    audio = MP3(audio_file)
+    return audio.info.length
 
 
 def calculate_wpm(word_count, duration):
     if duration == 0:
         return 0
-
     return word_count / (duration / 60)
 
 
 def speaking_speed_feedback(wpm):
-
     if wpm < 100:
-        return (
-            "🐢 Too Slow",
-            "Try speaking a little faster."
-        )
-
+        return "🐢 Too Slow", "Try speaking a little faster."
     elif wpm <= 160:
-        return (
-            "✅ Good",
-            "Excellent speaking speed!"
-        )
-
+        return "✅ Good", "Excellent speaking speed!"
     else:
-        return (
-            "🚀 Too Fast",
-            "Try slowing down slightly."
-        )
+        return "🚀 Too Fast", "Try slowing down slightly."
 
 
 def detect_filler_words(text):
-
     fillers = [
         "um",
         "uh",
@@ -49,11 +35,10 @@ def detect_filler_words(text):
         "actually",
         "basically",
         "so",
-        "well"
+        "well",
     ]
 
     text = text.lower()
-
     result = {}
 
     for word in fillers:
@@ -63,20 +48,15 @@ def detect_filler_words(text):
 
 
 def analyze_sentiment(text):
-
     blob = TextBlob(text)
-
     polarity = blob.sentiment.polarity
 
     if polarity > 0:
         sentiment = "😊 Positive"
-
     elif polarity < 0:
         sentiment = "😞 Negative"
-
     else:
         sentiment = "😐 Neutral"
 
     confidence = abs(polarity) * 100
-
     return sentiment, confidence
